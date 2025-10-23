af0f0fb307ad14f02f143ee56fcf1982476007f8
Brrrtmrt <69765218+Brrrtmrt@users.noreply.github.com>
2025-10-23 15:07:28 +0300

    Refactor SkipList to SkipSet with thread safety

--- SkipList/SkipList.h
+++ SkipList/SkipList.h
@@ -3,24 +3,28 @@
 #include <algorithm>
 #include <memory>
 #include <initializer_list>
+#include <mutex>
+#include <optional>
 #include <ostream>
 #include <random>
-#include "Iterator.h"
+#include "SkipNode.h"
+#include <shared_mutex>
 
 
 template<typename Key, typename T, typename Allocator=std::allocator<T> >
-class SkipList {
+class SkipSet {
     using Node = SkipNode<Key, T>;
     Node *root;
     Node *tail;
     int m_currLevel;
     int m_maxLevel;
     size_t m_size;
+    mutable std::shared_mutex mu;
 
     [[nodiscard]] int randomLevel() const {
-        static std::random_device rd;
-        static std::mt19937 gen(rd());
-        static std::geometric_distribution<> dst(0.5);
+        //implicitly static
+        thread_local std::mt19937 gen(std::random_device{}());
+        thread_local std::geometric_distribution<> dst(0.5);
         int lvl{0};
         while (dst(gen) == 0 && lvl < m_maxLevel) {
             lvl++;
@@ -33,47 +37,38 @@ public:
     using NodeTraits = std::allocator_traits<NodeAlloc>;
     NodeAlloc m_alloc;
 
-    using iterator = Iterator<false, Key, T>;
-    using const_iterator = Iterator<true, Key, T>;
-
-    iterator begin() noexcept { return iterator(root->forward[0]); }
-    iterator end() noexcept { return iterator(nullptr); }
-
-    [[nodiscard]] const_iterator begin() const noexcept { return const_iterator(root->forward[0]); }
-    [[nodiscard]] const_iterator end() const noexcept { return const_iterator(nullptr); }
-
-    [[nodiscard]] const_iterator cbegin() const noexcept { return const_iterator(root->forward[0]); }
-    [[nodiscard]] const_iterator cend() const noexcept { return const_iterator(nullptr); }
-
-
-    explicit SkipList()
-        : m_maxLevel{16}, m_currLevel{0}, m_size{0} {
+    explicit SkipSet()
+        : m_currLevel{0}, m_maxLevel{16}, m_size{0} {
         root = NodeTraits::allocate(m_alloc, 1);
         NodeTraits::construct(m_alloc, root, Key{}, T{}, 16);
         tail = nullptr;
-        std::cout << m_maxLevel << " ";
     }
 
-    SkipList(std::initializer_list<std::pair<Key, T> > init)
-        : SkipList() {
+    SkipSet(std::initializer_list<std::pair<Key, T> > init)
+        : SkipSet() {
         for (auto &&p: init) {
             insert(p.first, p.second);
         }
     }
 
-    ~SkipList() {
+    ~SkipSet() {
         clear();
         NodeTraits::destroy(m_alloc, root);
         NodeTraits::deallocate(m_alloc, root, 1);
     }
 
-    [[nodiscard]] constexpr bool empty() const noexcept { return size() == 0; }
+    [[nodiscard]] bool empty() const noexcept {
+        std::shared_lock lock(mu);
+        return m_size == 0;
+    }
 
-    [[nodiscard]] constexpr size_t size() const noexcept {
+    [[nodiscard]] size_t size() const noexcept {
+        std::shared_lock lock(mu);
         return m_size;
     }
 
     void clear() noexcept {
+        std::unique_lock lock(mu);
         Node *current = root->forward[0];
         while (current) {
             Node *next = current->forward[0];
@@ -89,30 +84,33 @@ public:
         m_size = 0;
     }
 
-    T &front() {
-        return *begin();
+
+    T front() const {
+        std::shared_lock lock(mu);
+        if (m_size == 0) {
+            throw std::runtime_error("Empty SkipSet");
+        }
+        return root->forward[0]->value;
     }
 
-    T &front() const {
-        return *begin();
-    }
-
-    T &back() {
-        return tail->value;
-    }
-
-
-    T &back() const {
+    T back() const {
+        std::shared_lock lock(mu);
+        if (m_size == 0) {
+            throw std::runtime_error("Empty SkipSet");
+        }
         return tail->value;
     }
 
     [[nodiscard]] size_t maxlevel() const noexcept {
+        std::shared_lock lock(mu);
         return m_maxLevel;
     }
 
-
+    /*TODO: "add false"
+     */
     template<typename U>
-    iterator insert(const Key &k, U &&v) {
+    bool insert(const Key &k, U &&v) {
+        std::unique_lock lock(mu);
         std::vector<Node *> update(m_maxLevel + 1, nullptr);
         Node *x = root;
 
@@ -128,7 +126,7 @@ public:
             if (x->forward[0] == nullptr) {
                 tail = x;
             }
-            return iterator(x);
+            return false;
         }
 
         int lvl{randomLevel()};
@@ -151,10 +149,11 @@ public:
             tail = tmp;
         }
 
-        return iterator(tmp);
+        return true;
     }
 
-    iterator find(const Key &k) {
+    std::optional<T> find(const Key &k) {
+        std::shared_lock lock(mu);
         Node *x = root;
         for (int i = m_currLevel; i >= 0; i--) {
             while (x->forward[i] != nullptr && (k > x->forward[i]->key)) {
@@ -163,12 +162,13 @@ public:
         }
         x = x->forward[0];
         if (x != nullptr && x->key == k) {
-            return iterator(x);
+            return x->value;
         }
-        return iterator(nullptr);
+        return {};
     }
 
-    const_iterator find(const Key &k) const {
+    std::optional<const T> find(const Key &k) const {
+        std::shared_lock lock(mu);
         Node *x = root;
         for (int i = m_currLevel; i >= 0; i--) {
             while (x->forward[i] != nullptr && (k > x->forward[i]->key)) {
@@ -177,28 +177,14 @@ public:
         }
         x = x->forward[0];
         if (x != nullptr && x->key == k) {
-            return const_iterator(x);
+            return x->value;
         }
-        return const_iterator(nullptr);
-    }
-
-    template<typename K>
-    iterator find(const K &k) {
-        Node *x = root;
-        for (int i = m_currLevel; i >= 0; i--) {
-            while (x->forward[i] != nullptr && (k > x->forward[i]->key)) {
-                x = x->forward[i];
-            }
-        }
-        x = x->forward[0];
-        if (x != nullptr && x->key == k) {
-            return iterator(x);
-        }
-        return iterator(nullptr);
+        return {};
     }
 
     template<typename K>
-    const_iterator find(const K &k) const {
+    std::optional<T> find(const K &k) {
+        std::shared_lock lock(mu);
         Node *x = root;
         for (int i = m_currLevel; i >= 0; i--) {
             while (x->forward[i] != nullptr && (k > x->forward[i]->key)) {
@@ -207,21 +193,54 @@ public:
         }
         x = x->forward[0];
         if (x != nullptr && x->key == k) {
-            return const_iterator(x);
+            return x->value;
         }
-        return const_iterator(nullptr);
+        return {};
+    }
+
+    template<typename K>
+    std::optional<const T> find(const K &k) const {
+        std::shared_lock lock(mu);
+        Node *x = root;
+        for (int i = m_currLevel; i >= 0; i--) {
+            while (x->forward[i] != nullptr && (k > x->forward[i]->key)) {
+                x = x->forward[i];
+            }
+        }
+        x = x->forward[0];
+        if (x != nullptr && x->key == k) {
+            return x->value;
+        }
+        return {};
     }
 
     bool contains(const Key &k) const {
-        return find(k) != end();
+        std::shared_lock lock(mu);
+        Node *x = root;
+        for (int i = m_currLevel; i >= 0; i--) {
+            while (x->forward[i] != nullptr && (k > x->forward[i]->key)) {
+                x = x->forward[i];
+            }
+        }
+        x = x->forward[0];
+        return (x != nullptr && x->key == k);
     }
 
     template<typename K>
     bool contains(const K &k) const {
-        return find(k) != end();
+        std::shared_lock lock(mu);
+        Node *x = root;
+        for (int i = m_currLevel; i >= 0; i--) {
+            while (x->forward[i] != nullptr && (k > x->forward[i]->key)) {
+                x = x->forward[i];
+            }
+        }
+        x = x->forward[0];
+        return (x != nullptr && x->key == k);
     }
 
-    iterator erase(const Key &k) {
+    void erase(const Key &k) {
+        std::unique_lock lock(mu);
         std::vector<Node *> update(m_maxLevel + 1, nullptr);
         Node *x = root;
         for (int i = m_currLevel; i >= 0; i--) {
@@ -230,7 +249,7 @@ public:
             update[i] = x;
         }
         x = x->forward[0];
-        if (!x || x->key != k) return end();
+        if (!x || x->key != k) return;
         for (int i = 0; i <= m_currLevel; ++i) {
             if (update[i]->forward[i] == x)
                 update[i]->forward[i] = x->forward[i];
@@ -240,23 +259,21 @@ public:
         if (x->forward[0] == nullptr) {
             tail = update[0] == root ? nullptr : update[0];
         }
-        Node *next = x->forward[0];
         NodeTraits::destroy(m_alloc, x);
         NodeTraits::deallocate(m_alloc, x, 1);
         --m_size;
         while (m_currLevel > 0 && !root->forward[m_currLevel])
             --m_currLevel;
-
-        return next == nullptr ? end() : iterator(next);
     }
 
     //not good
     void print() const {
+        std::shared_lock lock(mu);
         for (int lvl = m_currLevel; lvl >= 0; --lvl) {
             Node *curr = root->forward[lvl];
             std::cout << "Level " << lvl << ": ";
             while (curr) {
-                std::cout << "[" << curr->key << ":] -> ";
+                std::cout << "[" << "K:" << curr->key << ":" << "V:" << curr->value << "] -> ";
                 curr = curr->forward[lvl];
             }
             std::cout << "nullptr\n";
